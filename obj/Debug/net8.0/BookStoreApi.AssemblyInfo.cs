d33f5097cfadb5de192806ae2bbac139e68bdd7e
Varan Gulati <varangulati012@gmail.com>
2024-03-15 22:30:33 +0530

    add gitignore

--- obj/Debug/net8.0/BookStoreApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookStoreApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba238c80732a2b91c31ddf84057aaddfe67945a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7872e87039e4ac6ffb3f0c21564af42bb368aa61")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
